ef200acbf960a53a14e1fb771cd4433809d355a5
adamaslan <chillcoders@gmal.com>
2025-11-27 13:34:28 -0600

    nu-g

---
+++ universes.py
@@ -0,0 +1,22 @@
+UNIVERSES = {
+    "sp500": [
+        "AAPL", "MSFT", "GOOGL", "AMZN", "NVDA", "META", "TSLA", "BRK.B",
+        "UNH", "XOM", "JNJ", "JPM", "V", "PG", "MA", "HD", "CVX", "MRK",
+        # ... 500 total symbols
+    ],
+    
+    "nasdaq100": [
+        "AAPL", "MSFT", "GOOGL", "AMZN", "NVDA", "META", "TSLA", "AVGO",
+        "COST", "NFLX", "AMD", "PEP", "ADBE", "CSCO", "CMCSA", "TMUS",
+        # ... 100 total symbols
+    ],
+    
+    "etf_large_cap": [
+        "SPY", "VOO", "IVV", "VTI", "QQQ", "DIA", "IWM", "VEA", "VWO",
+        "EFA", "IEFA", "AGG", "BND", "VIG", "VYM", "SCHD", "VUG", "VTV"
+    ],
+    
+    "etf_sector": [
+        "XLK", "XLF", "XLV", "XLE", "XLY", "XLP", "XLI", "XLB", "XLU", "XLRE"
+    ]
+}
